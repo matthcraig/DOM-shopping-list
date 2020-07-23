c35a7201cd5e0152da967161eb70001b24837963
Matthew Craig <mgcraig78@gmail.com>
2020-07-23 14:43:31 -0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+
+button.addEventListener("click", function(){
+  console.log("click is working");
+})
