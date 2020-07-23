ad2845298f0b6a6e434f55f89980be3a4f805c17
Matthew Craig <mgcraig78@gmail.com>
2020-07-23 14:54:53 -0700

    add document method to li element

--- scripts.js
+++ scripts.js
@@ -2,5 +2,6 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 
 button.addEventListener("click", function(){
-  console.log("click is working");
+  var li = document.createElement("li");
+  li.prependChild(document.createTextNode("testing"));
 })
