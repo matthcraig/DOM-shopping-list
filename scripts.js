acdb8bb7c4877556bda4488ec5609d8b72258bb1
Matthew Craig <mgcraig78@gmail.com>
2020-07-27 13:08:45 -0700

    debug, fix delete button issue

--- scripts.js
+++ scripts.js
@@ -1,45 +1,61 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var li = document.getElementsByTagName("li");
+
+
+var button = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul');
+var li = document.getElementsByTagName('li');
+
+button.addEventListener('click', Onclick);
+input.addEventListener('keypress', addListAfterKeypress);
+
+listItems();
+buttonListElement();
 
 function inputLength(){
   return input.value.length;
 }
 function createListElement(){
-  var li = document.createElement("li");
+  var li = document.createElement('li');
+  var button = document.createElement('button');
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
+  li.appendChild(button);
+  button.innerHTML = "Delete";
   input.value = "";
-  var btn = document.createElement("button");
-  btn.appendChild(document.createTextNode("Delete"));
-  li.appendChild(btn);
-  btn.onclick = removeParent;
+  buttonListElement();
   listItems();
-}
-function removeParent(event){
-  event.target.parentNode.remove();
-}
+};
 
-function addListAfterClick(){
-    if (inputLength() > 0){
-      createListElement()
+function Onclick(){
+    if(inputLength() > 0){
+      createListElement();
     }
   }
 
-function addListAfterKeypress(event){
-  if (inputLength() > 0 && event.keyCode === 13){
-    createListElement()
+  function addListAfterKeypress(){
+  if(inputLength() > 0 && event.keyCode === 13){
+    createListElement();
   }
 }
+
 function listItems(){
   for (i=0; i<li.length; i++){
     li[i].addEventListener('click', changeClass)
   }
 }
-function changeClass(){
-  this.classList.toggle("clicked");
-}
-button.addEventListener("click", createListElement);
 
-input.addEventListener("keypress", addListAfterKeypress);
+function changeClass(){
+  this.classList.toggle('clicked');
+}
+
+function buttonListElement(){
+  var button = document.querySelectorAll('li button');
+  for( i=0; i<button.length; i++){
+  button[i].addEventListener('click', clearElement)
+  }
+}
+function clearElement(){
+  for(var i=0; i<li.length; i++){
+    this.parentNode.remove()
+  }
+};
